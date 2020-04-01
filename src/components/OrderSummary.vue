89d038356213ce2a678741b2d62f58558cfbc95b
thomas <t_huynh927@yahoo.com>
2020-04-01 00:12:22 -0700

    order summary in progress

--- src/components/OrderSummary.vue
+++ src/components/OrderSummary.vue
@@ -1,5 +1,18 @@
 <template>
-    <div></div>
+    <div class="card">
+      <h4 class="card-title">Order Summary</h4>
+      <div class="card-body">
+        <div class="d-flex justify-content-between">
+            <p>Subtotal</p><p>$70</p>
+        </div>
+        <div class="d-flex justify-content-between">
+            <p>Shipping to *user location*</p><p>$10</p>
+        </div>
+        <div class="d-flex justify-content-between">
+            <p>Tax</p><p>$7</p>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
